08bb89395531adf7efa9bb9771d8c624d7a9ae6f
dcodetuyyi <tuyyul460@gmail.com>
2025-06-30 16:50:30 +0800

    Add files via upload

--- utlis/adminAuth.js
+++ utlis/adminAuth.js
@@ -1,1 +1,13 @@
+import jwt from "jsonwebtoken";
 
+export function verifyAdmin(req, res) {
+  const { authorization } = req.headers;
+  if (!authorization) return null;
+  try {
+    const token = authorization.replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded;
+  } catch (e) {
+    return null;
+  }
+}
